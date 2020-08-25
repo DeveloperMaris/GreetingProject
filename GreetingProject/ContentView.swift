3c4cbdd823dfdc13b817cefea62acea1fade19d8
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2020-08-25 11:09:31 +0300

    Initial Commit

---
+++ GreetingProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GreetingProject
+//
+//  Created by Maris Lagzdins on 25/08/2020.
+//  Copyright © 2020 Maris Lagzdins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
